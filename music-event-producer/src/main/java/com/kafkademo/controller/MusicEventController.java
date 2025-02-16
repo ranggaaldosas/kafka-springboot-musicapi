c75c412598f3da043a1d386181157e8902934dac
Rangga Aldo <ranggaaldosas@gmail.com>
2025-02-16 22:40:46 +0700

    feat: producer config & acks

--- music-event-producer/src/main/java/com/kafkademo/controller/MusicEventController.java
+++ music-event-producer/src/main/java/com/kafkademo/controller/MusicEventController.java
@@ -1,26 +1,33 @@
 package com.kafkademo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafkademo.domain.MusicEvent;
+import com.kafkademo.producer.MusicEventProducer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // defining a class is a controller
+@RestController
 @Slf4j
 public class MusicEventController {
-    @PostMapping("/v1/music-event")
 
+    @Autowired
+    MusicEventProducer musicEventProducer;
+
+
+    @PostMapping("/v1/music-event")
     public ResponseEntity<MusicEvent> postMusicEvent(
             @RequestBody MusicEvent musicEvent
-    ) {
+    ) throws JsonProcessingException {
 
         log.info("Received music event request: {}", musicEvent);
 
-        // invoke kafka producer
-        log.info("Sending music event to kafka producer");
+        musicEventProducer.sendMusicEvent(musicEvent);
+
         log.info("Returning response: {}", musicEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(musicEvent);
     }
